cd8b5059b41d78cafc5a57d78152985048101c3f
Lucas Santos <lucas.santos.silva2011@gmail.com>
2018-03-15 10:42:19 -0300

    users: added user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { is_expected.to respond_to :id }
+  it { is_expected.to respond_to :name }
+  it { is_expected.to respond_to :email }
+  it { is_expected.to respond_to :birthdate }
+  it { is_expected.to respond_to :phone_number }
+end
