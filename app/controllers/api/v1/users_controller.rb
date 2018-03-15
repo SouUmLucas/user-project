26bed69483001447aa5851fe8b2c2aadc6843d8e
Lucas Santos <lucas.santos.silva2011@gmail.com>
2018-03-15 12:41:44 -0300

    cache: adding cache system through etag

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,11 +3,15 @@ class Api::V1::UsersController < ApplicationController
 
   def index
     users = User.all
-    render json: users, status: :ok
+    if stale? etag: users
+      render json: users, status: :ok
+    end    
   end
 
   def show
-    render json: @user, status: :ok
+    if stale? etag: @user
+      render json: @user, status: :ok
+    end
   end
 
   def create
